c4e6b0036b1d586fc5efbea17682271e5ef975b3
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2023-06-17 15:13:00 +0300

    fixed error

--- src/middlewares/rateLimitingMiddleware.ts
+++ src/middlewares/rateLimitingMiddleware.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 export const rateLimitingMiddleware = rateLimit({
 	windowMs: 1000 * 10,
-	max: 5,
+	max: 4,
 	message: "Too many attempts",
 	standardHeaders: true,
 	legacyHeaders: false,
